c15fc4ae1e820dd0429c801a115de5589d553262
Asim Habib <de.asimhabib@gmail.com>
2023-01-05 05:08:07 +0500

    updated about page with dynamic data from Realtor model and updated listings/urls.py

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
 # Create your views here.
 
 def index(request):
-    return render(request,'pages/index.html')
+    latest_listings=Listing.objects.order_by('-list_date')[:3]
+    context={
+        'latest_listings':latest_listings
+    }
+    return render(request,'pages/index.html',context)
 
 
 def about(request):
-    return render(request,'pages/about.html')
+    realtorsAreMVP=Realtor.objects.filter(is_mvp=True)
+    context={
+        'realtorsAreMVP':realtorsAreMVP
+    }
+    return render(request,'pages/about.html',context)
